1fbec6b08d72a13c10fb760929eda8394b0b6ab1
leinore <116540768+leinore@users.noreply.github.com>
2022-11-21 13:37:45 +0300

    add task_11

---
+++ task_11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число 
+// и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число {number}");
+
+int numberA = number/100;
+int numberB = number%10;
+
+Console.WriteLine($"{numberA*10 + numberB}");
